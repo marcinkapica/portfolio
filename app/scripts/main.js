1fe0e088be7d72ff1afb8397b8fcd952ebe3aecc
martions <martions@gmail.com>
2017-10-03 15:09:42 +0200

    changed language to english, removed empty .scss files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,12 +18,12 @@ function navbarBackground(){
 }
 
 function smoothScroll() {
-    jQuery('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = jQuery(this.hash);
-            target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                jQuery('html, body').animate({
+                $('html, body').animate({
                     scrollTop: target.offset().top
                 }, 1000);
                 return false;
